a6b82c627ef2f73d07e79c260c71f272e6e8c6b9
Jack Cai <caixunshiren@gmail.com>
2021-06-15 11:04:00 -0400

    Update bots.py

--- scripts/bots.py
+++ scripts/bots.py
@@ -1,15 +1,20 @@
 from scripts.assets import Stock, Account
 
 class Moving_average_bot:
-  def __init__(self, stock, balance):
+  '''
+  past n day moving average bot
+
+  '''
+  def __init__(self, stock, balance, n = 20):
     self.stock = stock
     self.balance = balance
     self.shares = 0
+    self.n = n
 
   def make_decision (self):
     cur_price = self.stock.cur_price
     last_price = self.stock.past_price[0]
-    moving_average = self.moving_average
+    moving_average = self.stock.moving_average(self.stock.cur_price, self.n)
     if last_price < moving_average and cur_price > moving_average:
       return 'buy'
     elif last_price > moving_average and cur_price < moving_average:
